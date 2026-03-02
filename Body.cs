6616f56f87a97a47be519a397e5b75971a1153aa
kiloexabyte <vothanhdat278+1@gmail.com>
2026-03-01 23:06:00 -0600

    Simplify Body to use primary constructor

--- Body.cs
+++ Body.cs
@@ -3,26 +3,17 @@ using System.Numerics;
 
 namespace GravitySim;
 
-public class Body
+public class Body(Vector2 position, Vector2 velocity, float mass, float radius)
 {
-    public Vector2 Position;
-    public Vector2 Velocity;
-    public Vector2 Acceleration;
+    public Vector2 Position = position;
+    public Vector2 Velocity = velocity;
+    public Vector2 Acceleration = Vector2.Zero;
 
-    public float Mass;
-    public float Radius;
+    public float Mass = mass;
+    public float Radius = radius;
 
     public const int MaxTrailLength = 120;
     public readonly Queue<Vector2> Trail = new();
-
-    public Body(Vector2 position, Vector2 velocity, float mass, float radius)
-    {
-        Position = position;
-        Velocity = velocity;
-        Mass = mass;
-        Radius = radius;
-        Acceleration = Vector2.Zero;
-    }
 
     public void RecordTrail()
     {
